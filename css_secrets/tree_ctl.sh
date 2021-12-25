9b13816f49ed1bfdd48ffe35a5650faaac72c9e1
burnt43 <jms.crsn@gmail.com>
2021-12-25 18:46:31 -0500

    udpate script

--- css_secrets/tree_ctl.sh
+++ css_secrets/tree_ctl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# --------------------------------------------------------------------------------
+# Define Functions
+# --------------------------------------------------------------------------------
 function echo_error {
   local msg="$1"
   echo -e "[\033[0;31mERROR\033[0;0m] - $msg"
@@ -18,31 +21,36 @@ function echo_fail {
   echo -e "\033[0;31mFAIL\033[0;0m"
 }
 
+# --------------------------------------------------------------------------------
+# Handle Args
+# --------------------------------------------------------------------------------
 script_dir=$(dirname $0)
-path_to_create="$1"
-file_to_create="$2"
+subdirectory="$1"
+leaf_file="$2"
 
-if [[ ! -z "$path_to_create" ]]; then
+if [[ ! -z "$subdirectory" ]]; then
   # This is the relative url for the files we are creating. They should be
   # accessible from the webserver.
-  relative_url_path="/css_secrets/${path_to_create}"
-  path_to_create="${script_dir}/${path_to_create}"
+  relative_url_path="/css_secrets/${subdirectory}"
+  subdirectory="${script_dir}/${subdirectory}"
 
   # Try and create the path.
-  echo_proc "mkdir -p $path_to_create"
-  mkdir -p $path_to_create
+  echo_proc "mkdir -p $subdirectory"
+  mkdir -p $subdirectory
   ([[ "$?" == "0" ]] && echo_ok) || echo_fail
 
-  # If there is a file to create, then create it.
-  if [[ ! -z "$file_to_create" ]]; then
+  # --------------------------------------------------------------------------------
+  # Write HTML & CSS File
+  # --------------------------------------------------------------------------------
+  if [[ ! -z "$leaf_file" ]]; then
     # Ensure the css file exists.
-    css_file_to_create="${path_to_create}/${file_to_create}.css"
+    css_file_to_create="${subdirectory}/${leaf_file}.css"
     echo_proc "touch $css_file_to_create"
     touch $css_file_to_create
     ([[ "$?" == "0" ]] && echo_ok) || echo_fail
 
     # Ensure the html files exists.
-    html_file_to_create="${path_to_create}/${file_to_create}.html"
+    html_file_to_create="${subdirectory}/${leaf_file}.html"
     echo_proc "touch $html_file_to_create"
     touch $html_file_to_create
     ([[ "$?" == "0" ]] && echo_ok) || echo_fail
@@ -52,7 +60,7 @@ if [[ ! -z "$path_to_create" ]]; then
     cat > $html_file_to_create <<-EOF
 <html>
   <head>
-    <link rel="stylesheet" href="${relative_url_path}/${file_to_create}.css">
+    <link rel="stylesheet" href="${relative_url_path}/${leaf_file}.css">
   </head>
   <body>
     <h1>Change This</h1>
@@ -62,15 +70,17 @@ EOF
     ([[ "$?" == "0" ]] && echo_ok) || echo_fail
   fi
 
-  # Ensure the index file exists.
-  index_file_to_create="${path_to_create}/index.html"
+  # --------------------------------------------------------------------------------
+  # Write Subdir Index
+  # --------------------------------------------------------------------------------
+
+  index_file_to_create="${subdirectory}/index.html"
   echo_proc "touch $index_file_to_create"
   touch $index_file_to_create
   ([[ "$?" == "0" ]] && echo_ok) || echo_fail
 
-  # Update the index page to include all the subpages.
   list=""
-  for file in $(find $path_to_create -type f -name "*.html" ! -name "index.html" | sort); do
+  for file in $(find $subdirectory -type f -name "*.html" ! -name "index.html" | sort); do
     name=$(basename "$file" | cut -d. -f1)
     list="${list}<li><a href=\"${relative_url_path}/${name}.html\">${name}</a></li>"
   done
@@ -90,13 +100,16 @@ EOF
   ([[ "$?" == "0" ]] && echo_ok) || echo_fail
 fi
 
+# --------------------------------------------------------------------------------
+# Write Root Index
+# --------------------------------------------------------------------------------
+
 list=""
 for dir in $(find $script_dir -type d ! -name "." ! -name "img" | sort); do
   name=$(basename "$dir")
   list="${list}<li><a href=\"/css_secrets/${name}\">${name}</a></li>"
 done
 
-# Update the root index to include all the subdirs.
 echo_proc "writing root index"
 cat > "${script_dir}/index.html" <<-EOF
 <html>
